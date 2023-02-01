68f193f91e7dda2905e467489a7c6e2e8400951f
Boboaca Costel <bobyca2003@yahoo.com>
2023-02-01 16:15:15 +0100

    Add files via upload

---
+++ blink-w600-pico/blink-w600-pico.ino
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+#define PIN_A0 (WM_IO_PA_00) //onboard led w600-pico
+
+void setup() {
+  pinMode(PIN_A0, OUTPUT);
+}
+
+void loop() {
+
+digitalWrite(PIN_A0, HIGH);
+delay(1000);
+digitalWrite(PIN_A0, LOW);
+delay(1000);
+
+}
+
