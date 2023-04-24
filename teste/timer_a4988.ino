49e2b3dbf75fcb4a38640e87a65c6103e3b997c1
Boboaca Costel <bobyca2003@yahoo.com>
2023-04-24 15:21:48 +0200

    Add files via upload

---
+++ teste/timer_a4988.ino
@@ -0,0 +1,19 @@
+#include <MsTimer.h>
+#include "Arduino.h"
+
+bool stare=TRUE
+
+void time_test_f(void)
+{
+  stare=!stare	
+  digitalWrite(PB_18, !stare);
+}
+
+void setup() {
+  pinMode(PB_18, OUTPUT); 
+  MsTimer1::set(1, time_test_f);
+  MsTimer1::start();
+  }
+  
+  void loop() {
+}
